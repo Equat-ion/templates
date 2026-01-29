71b09cfefa2574de53e5abcc8f6bcd43051aa11b
Equat-ion <syedareebahmad7100@gmail.com>
2026-01-29 18:27:05 +0530

    test

--- new_next_app.sh
+++ new_next_app.sh
@@ -4,7 +4,8 @@ function initialiseRepo() {
   git init $1
 }
 
-read -p "Enter the location to setup the project in [./]: " location
+read -p "Enter the name of your project: " projectName
+read -p "Enter the location to setup the project in [./]: " projectLocation
 read -p "Project type [web | python]: " projectType
 
 initialiseRepo $location
